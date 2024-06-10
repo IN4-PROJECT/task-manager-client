f0b9e92caf016b065f257fd2feba6f4377467a54
Djouko <destructeurkratos@gmail.com>
2024-06-10 11:05:57 +0100

    modifs du lien du backend
    modification du nouveau lien ou adresse pour acceder au backend

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   task_manager : {
-    API_URL : "http://127.0.0.1:8053/api",
-    URL_MEDIA : "http://localhost:8053/api/"
+    API_URL : "https://taskmanagerserver-production.up.railway.app/api",
+    URL_MEDIA : "https://taskmanagerserver-production.up.railway.app/api/"
   }
 };
